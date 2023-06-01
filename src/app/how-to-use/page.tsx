bbf0f16818f5afaf7e7c6db94c54caee44bf4560
markoclimako <mkclimako@gmail.com>
2023-06-01 07:58:13 -0300

    feat(page): create how to use page

---
+++ src/app/how-to-use/page.tsx
@@ -0,0 +1,10 @@
+import { Header } from "@/components/Header";
+
+export default function HowToUse() {
+  return (
+    <>
+      <Header/>
+      <h2 className='text-zinc-950'>How to use page</h2>
+    </>
+  );
+}
