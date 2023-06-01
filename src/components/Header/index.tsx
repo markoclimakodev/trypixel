3dd5ef33561ca14ac8e4f703b4f7135333e45f33
markoclimako <mkclimako@gmail.com>
2023-06-01 08:05:42 -0300

    page directories moved to pages folder

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,9 +5,9 @@ export function Header() {
     <header className='header'>
       <Logo />
       <nav className='flex gap-3 text-zinc-800'>
-        <a href='/playground' className='links link--blue'>Playground</a>
-        <a href='/how-to-use' className='links link--purple'>How to use</a>
-        <a href='/contact' className='links link--green'>Contact</a>
+        <a href='/pages/playground' className='links link--blue'>Playground</a>
+        <a href='/pages/how-to-use' className='links link--purple'>How to use</a>
+        <a href='/pages/contact' className='links link--green'>Contact</a>
       </nav>
     </header>
   );
