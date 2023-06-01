860660b25bfb2f907c6a96cd092f63ee8c522d72
markoclimako <mkclimako@gmail.com>
2023-06-01 08:00:19 -0300

    feat(page): create contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+import { Header } from "@/components/Header";
+
+export default function Contact() {
+  return (
+    <>
+      <Header/>
+      <h2 className='text-zinc-950'>Contact page</h2>
+    </>
+  );
+}
