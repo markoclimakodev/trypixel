d3db430e259c82d6d52608e433167a1fa2b75f0e
markoclimako <mkclimako@gmail.com>
2023-06-01 07:55:49 -0300

    feat(page): create playground page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,9 +5,9 @@ export function Header() {
     <header className='header'>
       <Logo />
       <nav className='flex gap-3 text-zinc-800'>
-        <a href='' className='links link--blue'>Playground</a>
-        <a href='' className='links link--purple'>How to use</a>
-        <a href='' className='links link--green'>Contact</a>
+        <a href='/playground' className='links link--blue'>Playground</a>
+        <a href='/how-to-use' className='links link--purple'>How to use</a>
+        <a href='/contact' className='links link--green'>Contact</a>
       </nav>
     </header>
   );
