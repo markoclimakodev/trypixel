78c0de7486be1c1c89f8f18ae6a44ce229568b20
markoclimako <mkclimako@gmail.com>
2023-08-09 05:27:37 -0300

    refactor: remove pages from navigation bar

--- src/app/pages/contact/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Header } from "@/components/Header";
-
-export default function Contact() {
-  return (
-    <>
-      <h2 className='text-zinc-950'>Contact page</h2>
-    </>
-  );
-}
--- src/app/pages/how-to-use/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Header } from '@/components/Header';
-
-export default function HowToUse() {
-  return (
-    <>
-      <h2 className='text-zinc-950'>How to use page</h2>
-    </>
-  );
-}
